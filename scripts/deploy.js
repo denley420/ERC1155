eb2972f830a0f8beb4dc3c28fc15c07151b86e8a
jakedenley <denley420@gmail.com>
2022-09-11 18:09:12 +0800

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const { parseEther } = require("ethers/lib/utils");
+
+async function deploy_ERC1155Token(){
+  console.log("Deploying address")
+  console.log("----------------------------------");
+  const factory = await ethers.getContractFactory("ERC1155Token");
+  
+  let contract = await factory.deploy("0xc9703E5041f6A009F6552BAc71Fd0aA053463ff3");
+
+  console.log("Contract Treasury: ", await contract.treasury());
+
+  console.log("Contract Address: ", contract.address)
+}
+
+deploy_ERC1155Token()
+.then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
